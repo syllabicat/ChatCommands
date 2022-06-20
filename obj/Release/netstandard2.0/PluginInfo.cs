c676ddcac79622792a9e44c72babe995df002a9b
syllabicat <107704955+syllabicat@users.noreply.github.com>
2022-06-20 12:08:19 -0700

    Remove generated files...

--- obj/Release/netstandard2.0/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace ChatCommands
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "ChatCommands";
-        public const string PLUGIN_NAME = "ChatCommands";
-        public const string PLUGIN_VERSION = "1.1.1";
-    }
-}
